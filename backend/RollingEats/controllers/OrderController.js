caa745d3c5c69e0d55ce0139d05fe6b29ee6dd21
N4choCM <nachocamposmarti@gmail.com>
2023-04-09 19:53:18 +0200

    ⬆ UPDATED: OrderController

---
+++ backend/RollingEats/controllers/OrderController.js
@@ -0,0 +1,94 @@
+const { response, request } = require("express");
+const Order = require("../models/Order");
+
+// Finds all the orders paginated.
+const getOrders = async (req = request, res = response) => {
+	const { from = 0, to = 5 } = req.query;
+	const query = { status: true };
+
+	const [total, orders] = await Promise.all([
+		Order.countDocuments(query),
+		Order.find(query).skip(Number(from)).limit(Number(to)),
+	]);
+
+	res.json({
+		total,
+		orders,
+	});
+};
+
+// Gets an order by its ID.
+const getOrderById = async (req = request, res = response) => {
+	const { id } = req.params;
+	const order = await Order.findById(id);
+	res.json({
+		order,
+	});
+};
+
+// Creates an Order.
+const createOrder = async (req, res = response) => {
+	const { date, delivered, status } = req.body;
+
+	// Generation of the data to be stored in the DB.
+	const data = {
+		user: req.user._id,
+		date,
+		menu: req.menu._id,
+		delivered,
+		status,
+	};
+
+	const order = new Order(data);
+	await order.save();
+	res.status(201).json({
+		order,
+		msg: "¡Pedido creado con éxito!",
+	});
+};
+
+// Updates an Order.
+const editOrder = async (req, res) => {
+	const { id } = req.params;
+	const { date, delivered, status } = req.body;
+  const user = req.user._id;
+  const menu = req.menu._id;
+
+	let data = {
+    user,
+		date,
+    menu,
+		delivered,
+		status,
+	};
+
+	const order = await Order.findByIdAndUpdate(id, data, { new: true });
+
+	res.status(201).json({
+		order,
+		msg: "Pedido actualizado correctamente.",
+	});
+};
+
+// Deletes an Order.
+const deleteOrderById = async (req, res) => {
+	const { id } = req.params;
+
+	const deletedOrder = await Order.findByIdAndUpdate(
+		id,
+		{ status: false },
+		{ new: true }
+	);
+
+	res.json({
+		msg: `El pedido del menú ${deletedOrder.menu.name} ha sido inactivado correctamente.`,
+	});
+};
+
+module.exports = {
+	getOrders,
+	getOrderById,
+	createOrder,
+	editOrder,
+	deleteOrderById,
+};
