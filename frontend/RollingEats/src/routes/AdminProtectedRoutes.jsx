2bfa7196851ab2cf4005078337d804f0233a454f
N4choCM <nachocamposmarti@gmail.com>
2023-04-21 01:32:44 +0200

    👾 FIXED: Bug in AdminProtectedRoutes.jsx

--- frontend/RollingEats/src/routes/AdminProtectedRoutes.jsx
+++ frontend/RollingEats/src/routes/AdminProtectedRoutes.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const AdminProtectedRoutes = ({ children, user }) => {
-  if (user.rol === "ADMIN_ROLE") {
+  if (user.role === "ADMIN_ROLE") {
     return children;
   } else {
     return <Navigate to="/" />;
