0cf53b48849fa068dcb6aadf63dc78842eef3342
N4choCM <nachocamposmarti@gmail.com>
2023-05-22 18:49:55 +0200

    👾 FIXED: getOrderByUser URL

--- frontend/RollingEats/src/helpers/OrderApi.js
+++ frontend/RollingEats/src/helpers/OrderApi.js
@@ -22,7 +22,7 @@ export const getOrders = async (limit = 15, page = 0) => {
 
 export const getOrdersByUser = async(user) => {
   try {
-    const resp = await fetch(url + "/" + user, {
+    const resp = await fetch(url + "/users/" + user, {
       method: "GET",
       headers: {
         "Content-type": "application/json; charset=UTF-8",
