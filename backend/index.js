3e3b87a035ca01ac6f3ed57e6cfb0bffcf3066a0
N4choCM <nachocamposmarti@gmail.com>
2023-06-06 02:36:25 +0200

    👾 FIXED: 81 unnecessary files removed...

--- backend/index.js
+++
@@ -1,6 +0,0 @@
-const Server = require("./models/Server");
-require("dotenv").config();
-
-const server = new Server();
-
-server.listen();
--- backend/routes/SearchRoute.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { search } = require("../controllers/SearchController");
-const { validateJWT } = require("../middlewares/JWTValidator");
-
-const router = Router();
-
-router.get("/:collection/:term", [validateJWT], search);
-
-module.exports = router;
